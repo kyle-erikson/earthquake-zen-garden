09d1b21e5739545d10918fc1788ce2af22e56d39
Kyle Erikson <kyle.d.erikson@gmail.com>
2021-08-07 20:44:36 -0600

    Start home page

---
+++ src/views/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// https://www.w3schools.com/howto/howto_js_sort_table.asp
+
+// https://www.smashingmagazine.com/2020/03/sortable-tables-react/
+
+const HomePage = () => {
+
+  return (
+    <h1>Home Page</h1>
+  )
+};
+
+export default HomePage;
